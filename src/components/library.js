b7434a454d6c733b0b2df5240fbfbf147b3b67be
jcouper <joshcouper@gmail.com>
2020-05-03 11:08:51 -0600

    library, book shelf, favorites components built. everything is wired up

---
+++ src/components/library.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from "react";
+
+
+
+
+export default class Library extends Component {
+    render(){
+        return(
+            <h1>This is working!</h1>
+        )
+    }
+}
